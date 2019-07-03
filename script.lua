63ca2b4ab589b59572088d5720de6a6e3ec62f8d
chekwob <chek@wobbyworks.com>
2019-07-03 01:27:29 +0000

    Add script printouts for C2 NTSC-U chunk loading

--- script.lua
+++ script.lua
@@ -182,6 +182,27 @@ do
     end
 end
 
+function printf(...)
+    print(string.format(...))
+end
+
+local eid_charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!"
+function eidtoname(value)
+    local char1 = ((value >> 25) & 0x3F) + 1
+    local char2 = ((value >> 19) & 0x3F) + 1
+    local char3 = ((value >> 13) & 0x3F) + 1
+    local char4 = ((value >> 7) & 0x3F) + 1
+    local char5 = ((value >> 1) & 0x3F) + 1
+
+    char1 = eid_charset:sub(char1, char1)
+    char2 = eid_charset:sub(char2, char2)
+    char3 = eid_charset:sub(char3, char3)
+    char4 = eid_charset:sub(char4, char4)
+    char5 = eid_charset:sub(char5, char5)
+
+    return char1 .. char2 .. char3 .. char4 .. char5
+end
+
 print "Hello, world!"
 
 -- Example: Change starting level of NTSC-U Crash 2 to Ruination
@@ -205,4 +226,31 @@ trapexec(0x8003A3B4, function()
         -- RNG values until the next different-PC RNG instruction.
         v1 = rng_counter - 20
     end
+end)
+
+-- Example: Print chunk load/unload messages
+trapexec(0x800125F8, function()
+    local chunkslot = a0
+    local chunkinfo_p = a0 * 44 + 0x800675B4
+    local chunk_p = readu32(chunkinfo_p)
+    local chunk_type = readu16(chunk_p + 2)
+    local chunk_id = readu32(chunk_p + 4)
+    if chunk_type == 1 then
+        chunk_id = eidtoname(chunk_id)
+    end
+    printf("CHUNK   LOAD @ slot %2d: chunk T%d; id %s", chunkslot, chunk_type, chunk_id)
+end)
+trapexec(0x80012A20, function()
+    local chunkslot = (a0 - 0x800675B4) // 44
+    local chunkinfo_p = a0
+    local chunk_p = readu32(chunkinfo_p)
+    local chunk_type = readu16(chunk_p + 2)
+    local chunk_id = readu32(chunk_p + 4)
+    if chunk_type == 1 then
+        chunk_id = eidtoname(chunk_id)
+    end
+    printf("CHUNK UNLOAD @ slot %2d: chunk T%d; id %s", chunkslot, chunk_type, chunk_id)
+end)
+trapexec(0x800126F4, function()
+    --print("LOADING ENTRY: " .. eidtoname(v0))
 end)

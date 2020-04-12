a4585a25167eb2d66a35bc8aeb3b27894e41f109
chekwob <chek@wobbyworks.com>
2020-04-12 22:16:03 +0000

    Remove joystick/gamecontroller init from main
    This fixes performance issues (occasional blocking) on some versions of
    SDL2 for linux.

--- main/main.c
+++ main/main.c
@@ -82,9 +82,7 @@ int main(int argc, char **argv)
 
     int err = SDL_Init(
         SDL_INIT_AUDIO |
-        SDL_INIT_VIDEO |
-        SDL_INIT_JOYSTICK |
-        SDL_INIT_GAMECONTROLLER
+        SDL_INIT_VIDEO
     );
     if (err < 0) {
         fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
